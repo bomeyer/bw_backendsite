d28490c292f2facd8f47a15c9c0487f704964bae
mark <mark>
2013-03-13 15:30:31 +0000

    initial import

--- mod1/conf.php
+++ mod1/conf.php
@@ -4,10 +4,3 @@ $myextconf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['bw_backe
 
 	// DO NOT REMOVE OR CHANGE THESE 2 LINES:
 $MCONF['name'] = $myextconf['menu']?$myextconf['menu']:'user'.'_txbwbackendsiteM1';
-$MCONF['script'] = '_DISPATCH';
-	
-$MCONF['access'] = 'user,group';
-
-$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
-$MLANG['default']['ll_ref'] = 'LLL:EXT:bw_backendsite/mod1/locallang_mod.xml';
-
